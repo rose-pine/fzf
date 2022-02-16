8c1557d688fdcfd0849f893a0ac02fc1fb418668
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2022-02-15 16:05:38 -0800

    Create fzf.sh

---
+++ fzf.sh
@@ -0,0 +1,5 @@
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+ --color=fg:#e0def4,bg:#1f1d2e,hl:#6e6a86
+ --color=fg+:#908caa,bg+:#191724,hl+:#908caa
+ --color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7
+ --color=marker:#ebbcba,spinner:#eb6f92,header:#ebbcba"
