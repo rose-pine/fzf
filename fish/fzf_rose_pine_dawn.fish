b8e8656e913f584f7ee92a3dc52ad0b89b0f2a8a
Thuan Pham <thuanowa@gmail.com>
2023-03-19 10:12:16 +0700

    feat: fish support (close #2)

---
+++ fish/fzf_rose_pine_dawn.fish
@@ -0,0 +1,5 @@
+set --global FZF_DEFAULT_OPTS "
+ --color=fg:#575279,bg:#fffaf3,hl:#9893a5
+ --color=fg+:#797593,bg+:#faf4ed,hl+:#797593
+ --color=info:#56949f,prompt:#56949f,pointer:#907aa9
+ --color=marker:#d7827e,spinner:#b4637a,header:#d7827e"
