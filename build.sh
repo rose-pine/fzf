9ea0ed0c1fd6ae53ce6c34ad7ccd773237ea028b
Joonatan <joonatanvark@gmail.com>
2024-04-12 16:09:39 +0300

    feat: add powershell support

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ echo "> Generating themes for sh..."
 npx @rose-pine/build@0.5.1 -t template.sh -o ./dist
 echo "> Generating themes for fish..."
 npx @rose-pine/build@0.5.1 -t template.fish -o ./dist
+echo "> Generating themes for powershell..."
+npx @rose-pine/build@0.5.1 -t template.ps1 -o ./dist
 echo "> Done. Themes can be found in ./dist"
 echo
---
+++ template.ps1
@@ -0,0 +1,7 @@
+$ENV:FZF_DEFAULT_OPTS = @"
+    --color=fg:$subtle,bg:$base,hl:$rose
+    --color=fg+:$text,bg+:$overlay,hl+:$rose
+    --color=border:$highlightMed,header:$pine,gutter:$base
+    --color=spinner:$gold,info:$foam,separator:$highlightMed
+    --color=pointer:$iris,marker:$love,prompt:$subtle
+"@
